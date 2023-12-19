5a5846ac900933c56a24379246fa86fcba446f2a
Gustav Syrén <g.e.syren@gmail.com>
2023-12-19 11:20:34 +0100

    update

--- apiBeer/dist/js/index.js
+++ apiBeer/dist/js/index.js
@@ -10,9 +10,9 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 const beerURL = 'https://api.punkapi.com/v2/beers?page=2&per_page=80';
 const randomButton = document.getElementById('random-button');
 const beerCard = document.querySelector('card');
-const beerImage = document.getElementById('beer-image');
-const beerName = document.getElementById('beer-name');
-const seeMoreButton = document.getElementById('see-more-button');
+const beerImages = document.querySelectorAll('.beer-image');
+const beerNames = document.querySelectorAll('.beer-name');
+const seeMoreButton = document.querySelectorAll('.see-more-button');
 const homeButton = document.getElementById('home-button');
 const searchMenuButton = document.getElementById('search-menu-button');
 const cardSection = document.getElementById('card-section');
@@ -20,6 +20,14 @@ const searchSection = document.getElementById('search-section');
 const inputField = document.querySelector('.input-field');
 const inputButton = document.querySelector('.input-button');
 const searchResultList = document.querySelector('.search-result-list');
+const mainSeeMoreButton = document.querySelector('#card-section > article > button.button.see-more-button');
+const description = document.querySelector('.description');
+const aBV = document.querySelector('.abv');
+const volume = document.querySelector('.volume');
+const ingredients = document.querySelector('.ingredients');
+const hops = document.querySelector('.hops');
+const foodPairing = document.querySelector('.food-pairing');
+const brewersTips = document.querySelector('.brewers-tips');
 function getBeer() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
@@ -31,6 +39,19 @@ function getBeer() {
                 });
                 inputButton.addEventListener('click', function (event) {
                     searchBeer(data);
+                });
+                seeMoreButton.forEach(button => {
+                    addEventListener('click', function (event) {
+                        const target = event.target;
+                        const card = target.closest('.card');
+                        if (card) {
+                            const beerNameElement = card.querySelector('.beer-name');
+                            if (beerNameElement) {
+                                const beerName = beerNameElement.textContent;
+                                seeMore(data, beerName);
+                            }
+                        }
+                    });
                 });
             }
             else {
@@ -44,10 +65,13 @@ function getBeer() {
 }
 function randomBeer(data) {
     const randomBeer = data[Math.floor(Math.random() * data.length)];
-    console.log(randomBeer);
-    beerImage.src = randomBeer.image_url;
-    beerName.innerHTML = randomBeer.name;
-    seeMoreButton.style.display = 'inline';
+    beerImages.forEach(element => {
+        element.src = randomBeer.image_url;
+    });
+    beerNames.forEach(element => {
+        element.innerHTML = randomBeer.name;
+    });
+    mainSeeMoreButton.style.display = 'inline';
 }
 getBeer();
 searchMenuButton.addEventListener('click', function (event) {
@@ -70,9 +94,28 @@ function searchBeer(data) {
             const li = document.createElement('li');
             const button = document.createElement('button');
             button.classList.add('input-button');
+            button.classList.add('see-more-button');
+            button.classList.add('beer-name');
             button.textContent = searchResult[i].name;
             li.appendChild(button);
             searchResultList.appendChild(li);
         }
     }
+}
+function seeMore(data, beerName) {
+    const beer = data.find(beer => beer.name === beerName);
+    console.log(beerName);
+    console.log(beer);
+    beerImages.forEach(element => {
+        element.src = beer.image_url;
+    });
+    beerNames.forEach(element => {
+        element.textContent = beer.name;
+    });
+    description.textContent = `Description: ${beer.description}`;
+    aBV.textContent = `Alcohol by volume: ${beer.abv}\n`;
+    volume.textContent = `Volume: ${beer.volume.value} ${beer.volume.unit}\n`;
+    ingredients.textContent = `Ingredients: Malt: ${beer.ingredients.malt[0].name}\n Hops:${beer.ingredients.hops[0].name}\n`;
+    foodPairing.textContent = `Food Pairing: ${beer.food_pairing}\n`;
+    brewersTips.textContent = `Brewer's tips: ${beer.brewers_tips}\n`;
 }
--- apiBeer/src/index.ts
+++ apiBeer/src/index.ts
@@ -63,9 +63,9 @@ interface Beer {
 const beerURL = 'https://api.punkapi.com/v2/beers?page=2&per_page=80'
 const randomButton = document.getElementById('random-button') as HTMLElement
 const beerCard = document.querySelector('card') as HTMLElement
-const beerImage = document.getElementById('beer-image') as HTMLImageElement
-const beerName = document.getElementById('beer-name') as HTMLElement
-const seeMoreButton = document.getElementById('see-more-button') as HTMLElement
+const beerImages = document.querySelectorAll('.beer-image') as NodeListOf<HTMLImageElement>;
+const beerNames = document.querySelectorAll('.beer-name') as NodeListOf<HTMLElement>;
+const seeMoreButton = document.querySelectorAll('.see-more-button') as NodeList
 const homeButton = document.getElementById('home-button') as HTMLElement
 const searchMenuButton = document.getElementById('search-menu-button') as HTMLElement
 const cardSection = document.getElementById('card-section') as HTMLElement
@@ -73,6 +73,14 @@ const searchSection = document.getElementById('search-section') as HTMLElement
 const inputField = document.querySelector('.input-field') as HTMLInputElement
 const inputButton = document.querySelector('.input-button') as HTMLElement
 const searchResultList = document.querySelector('.search-result-list') as HTMLElement
+const mainSeeMoreButton = document.querySelector('#card-section > article > button.button.see-more-button') as HTMLElement
+const description = document.querySelector('.description') as HTMLElement
+const aBV = document.querySelector('.abv') as HTMLElement
+const volume = document.querySelector('.volume') as HTMLElement
+const ingredients = document.querySelector('.ingredients') as HTMLElement
+const hops = document.querySelector('.hops') as HTMLElement
+const foodPairing = document.querySelector('.food-pairing') as HTMLElement
+const brewersTips = document.querySelector('.brewers-tips') as HTMLElement
 
 
 async function getBeer() {
@@ -80,11 +88,28 @@ async function getBeer() {
         const response = await fetch(beerURL)
         if (response.status === 200) {
             const data: Beer[] = await response.json();
+
             randomButton.addEventListener('click', function(event) {
                 randomBeer(data)
             })
             inputButton.addEventListener('click', function(event) {
                 searchBeer(data)
+            })
+
+            seeMoreButton.forEach(button => {
+                addEventListener('click', function(event) {
+                    const target = event.target as HTMLElement
+                    const card = target.closest('.card') as HTMLElement | null
+                    if (card) {
+                        const beerNameElement = card.querySelector('.beer-name') as HTMLElement | null;
+                        if (beerNameElement) {
+                            const beerName = beerNameElement.textContent;
+                            seeMore(data, beerName);
+                        }
+                    }
+    
+
+                })
             })
             
         }
@@ -99,10 +124,13 @@ async function getBeer() {
 
 function randomBeer(data: Beer[]) : void {
     const randomBeer = data[Math.floor(Math.random() * data.length)]
-    console.log(randomBeer)
-    beerImage.src = randomBeer.image_url
-    beerName.innerHTML = randomBeer.name
-    seeMoreButton.style.display = 'inline'
+    beerImages.forEach(element => {
+        element.src = randomBeer.image_url
+    })
+    beerNames.forEach(element => {
+        element.innerHTML = randomBeer.name
+    })
+    mainSeeMoreButton.style.display = 'inline'
 }
 
 getBeer();
@@ -131,10 +159,30 @@ function searchBeer(data) : void {
             const li = document.createElement('li')
             const button = document.createElement('button')
             button.classList.add('input-button')
+            button.classList.add('see-more-button')
+            button.classList.add('beer-name')
             button.textContent = searchResult[i].name
             li.appendChild(button)
             searchResultList.appendChild(li)
         }
     }
 
+}
+
+function seeMore(data, beerName): void {
+    const beer = data.find(beer => beer.name === beerName)
+    console.log(beerName)
+    console.log(beer)
+    beerImages.forEach(element => {
+        element.src = beer.image_url
+    })
+    beerNames.forEach(element => {
+        element.textContent = beer.name
+    })
+    description.textContent = `Description: ${beer.description}`
+    aBV.textContent = `Alcohol by volume: ${beer.abv}\n`
+    volume.textContent = `Volume: ${beer.volume.value} ${beer.volume.unit}\n`
+    ingredients.textContent = `Ingredients: Malt: ${beer.ingredients.malt[0].name}\n Hops: ${beer.ingredients.hops[0].name}\n`
+    foodPairing.textContent = `Food Pairing: ${beer.food_pairing}\n`
+    brewersTips.textContent = `Brewer's tips: ${beer.brewers_tips}\n`
 }
